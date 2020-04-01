382d33a731f79f477831eacda765086d6257dac1
leonardo l bufalo <leonardolsbufalo@gmail.com>
2020-04-01 13:02:35 -0300

    class classroom

--- oo/definindo_classe.rb
+++ oo/definindo_classe.rb
@@ -1,5 +1,20 @@
 class Conta
 
+    def depositar
+
+    end
+
+    def mostrarConta()
+        puts "Conta numero X"
+    end
+
+    def mensagem(mensagem = "default")
+        puts mensagem
+    end
+
+    def sum(a,b)
+        a+b
+    end
 end
 
 
@@ -7,3 +22,10 @@ obj1 = Conta.new
 
 
 puts obj1
+
+obj1.mostrarConta
+obj1.mensagem("Chamada metodo da classe conta")
+obj1.mensagem
+
+result = obj1.sum(5,6)
+puts result
